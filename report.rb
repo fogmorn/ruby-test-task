84d3e5550e5351051c5d9eb5b10f5659f173eac8
Grigory Kozlov <griga99@mail.ru>
2019-07-01 10:07:50 +0300

    Initial

---
+++ report.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+require 'json'
+
+class Report
+    attr_accessor :code, :guest, :entity, :type, :created_at, :updated_at
+
+    def read fname="shops.json"
+        open(fname).read
+    end
+
+    def parse json
+        JSON.parse(json)
+    end
+
+    def save
+        File.open("result", 'a') { |file|
+            file.write(
+                "#{@code}. Guest: #{@guest}. #{@type} #{@entity} at #{@updated_at}\n"
+            )
+        }
+    end
+
+    def send result_file, to="email"
+		report = open(result_file).read
+
+        case to
+        when "email"
+			Mailer.deliver(
+			  from: 'system@email.com', 
+			  to: host.email, 
+			  subject: 'Report',
+			  body: report
+			)
+        when "telegram"
+            #
+        end
+    end
+
+	def to_s
+		"#{@code}. Guest: #{@guest}. #{@type} #{@entity} at #{@updated_at}"
+	end
+        
+end
+
+r = Report.new
+parsed = r.parse(r.read)
+
+parsed.each do |object|
+    r.code =  object["code"]
+    r.guest = object["guest"]
+    r.entity =  object["entity"]
+    r.type = object["type"].capitalize
+    r.created_at = object["created_at"]
+    r.updated_at = object["updated_at"]
+    r.save
+
+	# Print string to stdout.
+    p r.to_s
+end
+
+# Send report
+# r.send("result_file", "email")
